ea88a3ad029f6764dbe74583fb516aed7cba3c65
Nathan Hogge <nhogge@gmail.com>
2018-02-26 07:48:49 -0500

    move team custom code to lib

---
+++ src/org/usfirst/frc3707/lib/pwinput/PWInput.java
@@ -0,0 +1,135 @@
+package org.usfirst.frc3707.lib.pwinput;
+
+import edu.wpi.first.wpilibj.Counter;
+import edu.wpi.first.wpilibj.DigitalSource;
+import edu.wpi.first.wpilibj.PIDSource;
+import edu.wpi.first.wpilibj.PIDSourceType;
+
+public class PWInput implements PIDSource {
+
+	// Though calibration offset was in the lidar code, I'm going to leave it out for now	
+	//private static final int CALIBRATION_OFFSET = 0;
+	
+	private Counter counter;
+	private double msPerUnit = 1;
+	private double maxPeriod = 1;
+	private double lastReading = 0;
+	private double ignoreValue = 0;
+	private int ignoreCount = 5;
+	private int maxIgnoreRun = 0;
+	public int ignoreIndex = 0;
+	public int runningIndex = 0;
+	
+	private int printedWarningCount = 5;
+	
+	public PWInput (DigitalSource source, double ignoreval, int ignorecount, double msperunit, double maxperiod) {
+		counter = new Counter(source);
+	    setMaxPeriod(maxperiod);
+	    // Configure for measuring rising to falling pulses
+	    counter.setSemiPeriodMode(true);
+	    counter.reset();
+	    setMsPerUnit(msperunit);
+	    setIgnoreValue(ignoreval);
+	    setIgnoreCount(ignorecount);
+	}
+	
+	public PWInput (DigitalSource source) {
+		this(source, 0, 5, 1, 1);
+	}
+	
+	public void setMsPerUnit (double newmsperunit) {
+		if (newmsperunit > 0) {
+			msPerUnit = newmsperunit;
+		}
+	}
+	
+	public double getmsperunit () {
+		return msPerUnit;
+	}
+	public double getMaxPeriod() {
+		return maxPeriod;
+	}
+	public void setMaxPeriod(double newmaxperiod) {
+		counter.setMaxPeriod(newmaxperiod);
+	}
+	public void setIgnoreValue (double newignoreval) {
+		ignoreValue = newignoreval;
+	}
+	public void setIgnoreValue (double newignoreval, int newignorecount) {
+		ignoreValue = newignoreval;
+		setIgnoreCount(newignorecount);
+	}
+	public void setIgnoreCount (int newic) {
+		if (newic > 0) {
+			ignoreCount = newic;
+		}
+	}
+	
+	public int getMaxIgnoreRun () {
+		return maxIgnoreRun;
+	}
+	//public double getRawMedian() {
+	//	
+	//}
+	//public double getMedian () {
+	//	
+	//}
+	
+	private double handleIgnore(double val) {
+		if (Math.abs(ignoreValue - val) < 0.5) {
+			ignoreIndex++;
+			runningIndex++;
+			if (maxIgnoreRun > 20) maxIgnoreRun = 0;
+			maxIgnoreRun = Math.max(maxIgnoreRun, runningIndex);
+			if (ignoreIndex == (ignoreCount + 1)) {
+				ignoreIndex = 0;
+			} else {
+				return(lastReading);
+			}
+		} else {
+			runningIndex = 0;
+		}
+		return(val);
+	}
+	public double getRawReading() {
+		double origMsPerUnit = msPerUnit;
+		double rval = 0;
+		msPerUnit = 1;
+		rval = getDistance();
+		msPerUnit = origMsPerUnit;
+		return (rval);
+	}
+		
+	
+	public double getDistance() {
+		double rval;
+		if (counter.get() < 1) {
+			if (printedWarningCount-- > 0) {
+				System.out.println("PWInput: waiting for distance measurement");
+			}
+			return 0;
+		}
+		rval = (counter.getPeriod() * 1000000.0 / msPerUnit);
+		rval = handleIgnore(rval);
+		lastReading = rval;
+		return rval;
+	}
+	
+	@Override
+	public void setPIDSourceType(PIDSourceType pidSource) {
+		
+	}
+	
+	@Override
+	public PIDSourceType getPIDSourceType() {
+		return PIDSourceType.kDisplacement;
+	}
+	
+	@Override
+	public double pidGet() {
+		double distance = getDistance();
+		System.out.println("DISTANCE");
+		System.out.println(distance);
+		return distance;
+	}
+}
---
+++ src/org/usfirst/frc3707/lib/swerve/SwerveDrive.java
@@ -0,0 +1,128 @@
+package org.usfirst.frc3707.lib.swerve;
+
+import edu.wpi.first.wpilibj.GyroBase;
+import edu.wpi.first.wpilibj.PIDOutput;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+public class SwerveDrive implements PIDOutput {
+
+    private SwerveWheel rightFrontWheel;
+    private SwerveWheel leftFrontWheel;
+    private SwerveWheel leftBackWheel;
+    private SwerveWheel rightBackWheel;
+    
+    private GyroBase gyro;
+
+    private double wheelbase = 22.5;
+    private double trackwidth = 24.5;
+    
+
+    public SwerveDrive(SwerveWheel rightFront, SwerveWheel leftFront, SwerveWheel leftBack, SwerveWheel rightBack, GyroBase gyro){
+	    	this.rightFrontWheel = rightFront;
+	    	this.leftFrontWheel = leftFront;
+	    	this.leftBackWheel = leftBack;
+	    	this.rightBackWheel = rightBack;
+	    
+        this.gyro = gyro;
+    }
+    
+    public void drive(double directionX, double directionY, double rotation, boolean useGyro, boolean slowSpeed) {
+    	
+//		SmartDashboard.putNumber("directionX", directionX);
+//		SmartDashboard.putNumber("directionY", directionY);
+//		SmartDashboard.putNumber("rotation", rotation);
+		 
+		//if BOTH joystick in the center
+		if((directionX < 0.2 && directionX > -0.2) && (directionY < 0.2 && directionY > -0.2) && (rotation < 0.2 && rotation > -0.2)) {
+			this.rightFrontWheel.updateSpeed(0);
+			this.leftFrontWheel.updateSpeed(0);
+			this.leftBackWheel.updateSpeed(0);
+			this.rightBackWheel.updateSpeed(0);
+			return;
+		}
+		//if ROTATION joystick only near the center (this fixes the rotation drift)
+		else if (rotation < 0.2 && rotation > -0.2) {
+			rotation = 0;
+		}
+		
+		double L = this.wheelbase; 					//distance between front and back wheels
+		double W = this.trackwidth; 				//distance between front wheels
+		double r = Math.sqrt ((L * L) + (W * W)); 	//radius of circle (actually it may be the diameter?)
+        
+        // directionY *= -1; 							//invert Y
+        // directionX *= -1; 							//invert X
+        // rotation *= -1;
+
+        double a = directionX - rotation * (L / r); //rear axle
+        double b = directionX + rotation * (L / r); //front axle
+        double c = directionY - rotation * (W / r); //left track
+        double d = directionY + rotation * (W / r); //right track
+        
+        /*
+         *                FRONT
+         * 
+         *            c          d
+         *            | 		 |
+         *       b ------------------ b
+         *            |          |
+         *            |          |
+         * LEFT       |          |      RIGHT
+         *            |          |
+         *            |          |
+         *       a ------------------ a
+         *            |          |
+         *            c          d
+         * 
+         *                BACK
+         */
+
+		//set motor speeds for each wheel
+		double backRightSpeed = Math.sqrt ((a * a) + (d * d));
+		double backLeftSpeed = Math.sqrt ((a * a) + (c * c));
+		double frontRightSpeed = Math.sqrt ((b * b) + (d * d));
+		double frontLeftSpeed = Math.sqrt ((b * b) + (c * c));
+
+		//set wheel angle for each wheel
+		double backRightAngle = (Math.atan2 (a, d) / Math.PI) * 180;
+		double backLeftAngle = (Math.atan2 (a, c) / Math.PI) * 180;
+		double frontRightAngle = (Math.atan2 (b, d) / Math.PI) * 180;
+		double frontLeftAngle = (Math.atan2 (b, c) / Math.PI) * 180;
+        
+        if(useGyro) {
+            double gyroAngle = normalizeGyroAngle(gyro.getAngle()); 
+            backRightAngle += gyroAngle;
+            backLeftAngle += gyroAngle;
+            frontRightAngle += gyroAngle;
+            frontLeftAngle += gyroAngle;
+        }
+        if(slowSpeed) {
+		    	backRightSpeed *= 0.5;
+		    	backLeftSpeed *= 0.5;
+		    	frontRightSpeed *= 0.5;
+		    	frontLeftSpeed *= 0.5;
+        }
+        
+        //update the actual motors
+		this.rightFrontWheel.drive(frontRightSpeed, frontRightAngle);
+		this.leftFrontWheel.drive(frontLeftSpeed, frontLeftAngle);
+		this.leftBackWheel.drive(backLeftSpeed, backLeftAngle);
+		this.rightBackWheel.drive(backRightSpeed, backRightAngle);
+    }
+    public void driveSimple(double speed, double angle) {
+		this.rightFrontWheel.drive(speed, angle);
+		this.leftFrontWheel.drive(speed, angle);
+		this.leftBackWheel.drive(speed, angle);
+		this.rightBackWheel.drive(speed, angle);
+    }
+
+	@Override
+	public void pidWrite(double output) {
+		System.out.println("X");
+		System.out.println(output);
+		driveSimple(output, 0.0);
+	}
+
+    public double normalizeGyroAngle(double angle){
+        return (angle - (Math.floor( angle / 360) * 360) );
+    }
+}
